02ba6d6a30a5940e2c13df96b8c06f8c5cf95eba
asdhbj <126579938+asdhbj@users.noreply.github.com>
2025-06-19 22:53:17 +0700

    Update js.js

--- js.js
+++ js.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
     measurementId: "G-TQM2CXTY6Y"
 };
 
-
+firebase.database().goOnline()
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig)
